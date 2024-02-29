65b6eafb64b6818243043d5147d8105a9d67c4d1
Ajabs9 <jacobhonour9@gmail.com>
2024-02-29 01:52:36 +0100

    Added the answers to the remaining questions and modified the javascript as needed for interaction

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const btnShow = document.getElementById('btnShow');
-    const addInfo = document.getElementById('addInfo');
-    const content = document.getElementById('content');
+const btnShow1 = document.getElementById('btnShow1');
+const btnShow2 = document.getElementById('btnShow2');
+const btnShow3 = document.getElementById('btnShow3');
+const btnShow4 = document.getElementById('btnShow4');
 
-    btnShow.addEventListener('click', function() {
-        if (content.style.display !== "block") {
-            addInfo.style.display = "block"
-            content.style.display = "block";
-            btnShow.style.background = "url(./images/icon-minus.svg)";
-            btnShow.style.backgroundRepeat = "no-repeat";
-            btnShow.style.backgroundPositionX = "right";
-        } else {
-            addInfo.style.display = "none";
-            content.style.display = "none";
-            btnShow.style.background = "url(./images/icon-plus.svg)";
-            btnShow.style.backgroundRepeat = "no-repeat";
-            btnShow.style.backgroundPositionX = "right";
-            console.log("It ran");
-        }
-    });
-});
+function contentDisplay(number){
+    const addInfo = document.getElementById('addInfo'+number);
+    const content = document.getElementById('content'+number);
+    const btnShow = document.getElementById('btnShow'+number);
+    if (content.style.display !== "block") {
+        addInfo.style.display = "block"
+        content.style.display = "block";
+        btnShow.style.background = "url(./images/icon-minus.svg)";
+        btnShow.style.backgroundRepeat = "no-repeat";
+        btnShow.style.backgroundPositionX = "right";
+    } else {
+        addInfo.style.display = "none";
+        content.style.display = "none";
+        btnShow.style.background = "url(./images/icon-plus.svg)";
+        btnShow.style.backgroundRepeat = "no-repeat";
+        btnShow.style.backgroundPositionX = "right";
+        console.log("It ran");
+    }
+}
